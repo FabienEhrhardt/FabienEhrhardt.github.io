3711af12cf1e192895587291ad6033269bb594d0
FabienEhrhardt <fabien.ehrhardt1@gmail.com>
2026-03-04 11:55:04 +0100

    Add files via upload

---
+++ exercices.js
@@ -0,0 +1,77 @@
+// fichier : exercices.js
+const listeExercices = [
+    {
+        theme: "0",
+        exercices: [
+
+        ]
+    },
+	{
+        theme: "1",
+        exercices: [
+
+        ]
+    },
+	{
+        theme: "2",
+        exercices: [
+
+        ]
+    },
+	{
+        theme: "31",
+        exercices: [
+            { nom: "exo1", titre: "Courbes sinusoïdales i(t) et v(t)" },
+			{ nom: "exo2", titre: "Tracé du diagramme de Fresnel pour circuit RL série" },
+        ]
+    },
+	{
+        theme: "32",
+        exercices: [
+            {},
+        ]
+    },
+	{
+        theme: "33",
+        exercices: [
+            {},
+        ]
+    },
+	{
+        theme: "41",
+        exercices: [
+		
+        ]
+    },
+	{
+        theme: "42",
+        exercices: [
+		
+        ]
+    },
+    {
+        theme: "5",
+        exercices: [
+            { nom: "exo1", titre: "Modèle de Thévenin d'une batterie" },
+        ]
+    },
+	{
+        theme: "6",
+        exercices: [
+            { nom: "exo1", titre: "Courbe I=f(V) d'un panneau photovoltaïque" },
+        ]
+    },
+	{
+        theme: "7",
+        exercices: [
+            { nom: "exo1", titre: "Courbe C=f(n) d'un moteur asynchrone" },
+            { nom: "exo2", titre: "Association d'un variateur et d'un moteur asynchrone" }
+        ]
+    },
+	{
+        theme: "8",
+        exercices: [
+            { nom: "exo1", titre: "Performances d'un asservissement" }
+        ]
+    }
+];
